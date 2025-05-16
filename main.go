dcf4d17800166788a800d07f8a12874f93dd34bf
Мария Попова <mariapopova@MacBook-Air-Maria.local>
2025-05-16 23:43:55 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +14,86 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		return []int{}
+	}
+	arr := make([]int, size)
+	for i := 0; i < size; i++ {
+		arr[i] = rand.Int()
+	}
+	return arr
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		panic("empty slice")
+	}
+
+	max := 0
+	for i := 0; i < len(data); i++ {
+		if data[i] > max {
+			max = data[i]
+		}
+	}
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		panic("empty slice")
+	}
+
+	chunkSize := len(data) / CHUNKS
+	if chunkSize == 0 {
+		return maximum(data)
+	}
+
+	var wg sync.WaitGroup
+	results := make(chan int, CHUNKS)
+
+	for i := 0; i < CHUNKS; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		if i == CHUNKS-1 {
+			end = len(data)
+
+			wg.Add(1)
+			go func(slice []int) {
+				defer wg.Done()
+				results <- maximum(slice)
+			}(data[start:end])
+		}
+
+		go func() {
+			wg.Wait()
+			close(results)
+		}()
+	}
+	max := <-results
+	for res := range results {
+		if res > max {
+			max = res
+		}
+	}
+
+	return max
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	arr := generateRandomElements(SIZE)
 
+	timeFirstWay := time.Now()
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	max := maximum(arr)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, time.Since(timeFirstWay).Microseconds())
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	timeSecondWay := time.Now()
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	max = maxChunks(arr)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, time.Since(timeSecondWay).Microseconds())
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,50 @@
 package main
 
-// Пишите тесты в этом файле
+import "testing"
+
+func TestGenerateRandomElements(t *testing.T) {
+	size := 100
+	arr := generateRandomElements(100)
+	lenOfArr := len(arr)
+	if lenOfArr <= 0 || lenOfArr < size {
+		t.Errorf("ожидаемый размер %d, получено %d", size, lenOfArr)
+	}
+
+	for _, v := range arr {
+		if v <= 0 {
+			t.Errorf("ожидаем положительное число, получено %d", v)
+		}
+	}
+}
+
+func TestMaximum(t *testing.T) {
+	size := 100
+	arr := generateRandomElements(size)
+	max := maximum(arr)
+	if max <= 0 {
+		t.Errorf("ожидаем положительное число, получено %d", max)
+	}
+
+	if len(arr) == 0 {
+		t.Errorf("ожидаемый размер %d, получено %d", size, 0)
+	}
+
+	if len(arr) == 1 && arr[0] != max {
+		t.Errorf("неверно определен максимум с одним элементом в списке, получено: %d, ожидалось: %d", max, arr[0])
+	}
+
+	arr = []int{5, 5, 5}
+	if maximum(arr) != 5 {
+		t.Errorf("неверно определен максимум списка из одинаковых элементов, получено: %d, ожидалось: %d", maximum(arr), 5)
+	}
+
+	arr = []int{10, 5, 5}
+	if maximum(arr) != 10 {
+		t.Errorf("максимум в начале списка определен неверно, получено: %d, ожидалось: %d", maximum(arr), 10)
+	}
+
+	arr = []int{5, 5, 10}
+	if maximum(arr) != 10 {
+		t.Errorf("максимум в конце списка определен неверно, получено: %d, ожидалось: %d", maximum(arr), 10)
+	}
+}
